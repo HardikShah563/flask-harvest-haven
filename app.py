111c0f83be268e108ce97ebab1a01ab8aef0e0a2
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-26 20:15:24 +0530

    admin operations

--- app.py
+++ app.py
@@ -155,9 +155,7 @@ def checkout():
         zip = data["zip"]
 
         msg = checkoutPurchase(session['u_id'], fullName, email, address, city, state, zip, total[3], session['cart'])
-        if(msg) == None: 
-            msgColor = "green"
-            msgText = "Checkout Successful!" 
+        if(msg) == None:
             reduceStock(display_cart)
             session['cart'] = initializeCart()
             display_cart = {}
@@ -167,7 +165,195 @@ def checkout():
 
 # -------------------------------------------------------
 
+@app.route('/admin-dashboard', methods=["GET"])
+def adminDashboard(): 
+    try:
+        allItems = getAllItemsFromDB()
+        allCategories = getAllCategories()
+        return jsonify({"categories": allCategories, "items": allItems}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
 
+# -------------------------------------------------------
+
+@app.route('/admin-stats')
+def adminStats():
+    try:
+        allItems = getAllItemsFromDB()
+        stats = {
+            "totalusers" : totalUsers(),
+            "male" : totalMaleUsers(),
+            "female" : (totalUsers() - totalMaleUsers()),
+            "orders"  : totalOrders(),
+            "totalsales" : totalSales(),
+            "salesRevenue" : totalSalesRevenue(),
+            "averageOrderValue" : averageOrderValue(),
+            "repeatPurchaseRate" : repeatPurchaseRate(),
+            "bestSellingProduct" : bestSellingProducts(),
+            "slowMovingProduct" : slowMovingProduct(),
+            "stockLevels" : stockLevels(),
+        }
+        return jsonify({"stats": stats, "items": allItems}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/add-item', methods=["GET", "POST"])
+def addItem():
+    try:
+        allCategories = getAllCategories()
+        if request.method == 'GET':
+            return jsonify({"categories": allCategories, }), 500
+        
+        if request.method == 'POST':
+            data = request.get_json()
+            pName = data['p_name']
+            pQty = data['p_qty']
+            pPrice = data['p_price']
+            pStockQty = data['p_stock_qty']
+            pImg = data['p_img'].read()
+            cID = data['c_id']
+
+            msg = putItems(pName, pQty, pPrice, pStockQty, pImg, cID)
+            if(msg == None):
+                getAllItemsFromDB()
+                return jsonify({"message": "New Item Created Successfully"}), 200
+            else:
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/edit-item', methods=["GET", "POST"])
+def editItem():
+    try:
+        allCategories = getAllCategories()
+        allItems = getAllItemNamesAndIDs()
+        if request.method == 'GET':
+            return jsonify({"items": allItems, "categories": allCategories}), 200
+
+        if request.method == 'POST':
+            data = request.get_json()
+            p_id = data["p_id"]
+            new_name = data["new_name"]
+            c_id = data["c_id"]
+            p_qty = data["p_qty"]
+            p_price = data["p_price"]
+            p_stock_qty = data["p_stock_qty"]
+
+            productDetails = getProductFromID(p_id)
+            if c_id == None: 
+                c_id = productDetails[6]
+            
+            if p_qty == None:
+                p_qty = productDetails[2]
+
+            if p_stock_qty == None:
+                p_stock_qty = productDetails[4]
+            
+            if p_price == None: 
+                p_price = productDetails[3]
+
+            msg = editItemDetails(c_id, p_id, new_name, p_qty, p_price, p_stock_qty)
+            if(msg == None):
+                return jsonify({"message": "New Item Created Successfully"}), 200
+
+            else:
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/delete-item', methods=["GET", "POST"])
+def deleteItem():
+    try:
+        allItems = getAllItemNamesAndIDs()
+        if request.method == "GET":
+            return jsonify({"items": allItems}), 200
+        
+        if request.method == "POST": 
+            data = request.get_json()
+            p_id = data["p_id"]
+            msg = deleteProduct(p_id)
+            if(msg == None): 
+                return jsonify({"message": "Item Deleted Successfully"}), 200
+
+            else: 
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/add-category', methods=["GET", "POST"])
+def addCategory():
+    try:
+        if request.method == "POST":
+            data = request.get_json()
+            c_name = data["c_name"]
+            msg = addNewCategory(c_name)    
+            if(msg == None): 
+                getAllItemsFromDB()
+                return jsonify({"message": "New Category Created Successfully"}), 200
+
+            else: 
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/edit-category', methods=["GET", "POST"])
+def editCategory():
+    try:
+        allCategories = getAllCategories()
+        if request.method == "GET":
+            return jsonify({"categories": allCategories}), 200
+
+        if request.method == "POST":
+            data = request.get_json()
+            oldID = data["old_name"]
+            newName = data["new_name"]
+            msg = editCategoryName(oldID, newName)
+            if(msg == None): 
+                return jsonify({"message": "Category Updated Successfully"}), 200
+
+            else: 
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
+
+# -------------------------------------------------------
+
+@app.route('/delete-category', methods=["GET", "POST"])
+def deleteCategory():
+    try:
+        allCategories = getAllCategories()
+        if request.method == "GET":
+            return jsonify({"categories": allCategories}), 200
+
+        if request.method == "POST":
+            data = request.get_json()
+            c_id = data["c_id"]
+            msg = deleteCategoryCompletely(c_id)
+            if(msg == None): 
+                return jsonify({"message": "Category Deleted Successfully"}), 200
+
+            else: 
+                return jsonify({"error": "Internal Server Error"}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # -------------------------------------------------------
 # function to sign out user, clear the session variables
